dd3b54f76f6ed30c28bc3635edcf063c6b19fedc
Yifei Wei <yifeiwei7@outlook.com>
2025-09-03 23:14:13 +1000

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+HOST_ALIAS = "xxx.xxxx.xx"
+CHECK_INTERVAL = 3.0               # checking interval (seconds)
+CONFIRM_FAILS = 3                  # number of failed checks to trigger alarm
+SOUND = "/System/Library/Sounds/Hero.aiff"
